16b4af2393dde1c3b40656db5bad7e2246aaecec
shc-0 <ishashwat@tutanota.com>
2023-02-08 14:30:55 +0530

    connect MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 
-app.listen(3000, () => {
+dotenv.config(); 
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("MongoDB Connected");
+}).catch(err => {
+    console.log(err); 
+});
+
+app.listen(8800, () => {
     console.log("Backend server is running");
 })
