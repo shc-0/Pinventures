48b6376505d7eb350ce16e967bb78fd161ee89de
shc-0 <ishashwat@tutanota.com>
2023-02-09 13:35:25 +0530

    Create Nodejs REST API

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const pinRoutes = require("./routes/pins")
 
 dotenv.config(); 
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("MongoDB Connected");
-}).catch(err => {
-    console.log(err); 
-});
+}).catch(err => console.log(err));
+
+app.use("/api/pins", pinRoutes);
 
 app.listen(8800, () => {
     console.log("Backend server is running");
